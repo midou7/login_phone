b114bc77605c26ea859f914adabe417b77d67ddc
Midou <mehdi.rouan@gmail.com>
2020-06-24 15:08:18 +0100

    initial commit

---
+++ lib/phone_login.dart
@@ -0,0 +1,97 @@
+
+import 'package:flutter/material.dart';
+import 'package:login_phone/otp_screen.dart';
+
+
+
+class PhoneLoginScreen extends StatelessWidget {
+  final _phoneController = TextEditingController(text: '665272833');
+  final _passController = TextEditingController(text: 'azerty');   
+
+ 
+ 
+ 
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(32),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Login", style: TextStyle(color: Colors.lightBlue, fontSize: 36, fontWeight: FontWeight.w500),),
+
+                SizedBox(height: 16,),
+
+                TextFormField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide(color: Colors.grey[200])
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide(color: Colors.grey[300])
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                      hintText: "Phone Number",
+                  ),
+                  controller: _phoneController,
+                ),
+
+                SizedBox(height: 16,),
+
+                TextFormField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide(color: Colors.grey[200])
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide(color: Colors.grey[300])
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                      hintText: "Password"
+
+                  ),
+
+                  controller: _passController,
+                ),
+
+                SizedBox(height: 16,),
+
+                Container(
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: Text("Login"),
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    onPressed: (){
+                          print('i was pressed !!');
+                        //code for sign in
+                        final phone = _phoneController.text.trim();
+                            Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        OTPScreen(
+                                                      mobileNumber:
+                                                         phone,
+                                                    ),
+                                                  ));         
+                    },
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
